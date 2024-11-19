9ccdfdd115c65f9a313daad76d9cd4c69179455a
KristianHeggdal <kristianheggdal@hotmail.no>
2024-11-19 16:41:34 +0100

    Add project files.

---
+++ Emne 3 ukedager/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hvilken dag er det i dag? skriv et tall");
+var tall=Console.ReadLine();
+switch (tall)
+{
+    case "1":
+        Console.WriteLine("Det er Mandag");
+        break;
+    case "2":
+        Console.WriteLine("Det er tirsdag");
+        break;
+    case "3":
+        Console.WriteLine("Det er Onsdag");
+        break;
+    case "4":
+        Console.WriteLine("Det er Torsdag");
+        break;
+    case "5":
+        Console.WriteLine("Det er Fredag");
+        break;
+    case "6":
+        Console.WriteLine("Det er Lørdag");
+        break;
+    case "7":
+        Console.WriteLine("Det er Søndag");
+        break;
+
+} 
